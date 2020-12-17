072bfc4986efef3fe2a240c803407d5e116cf134
EvanErickson <evanserickson@gmail.com>
2020-12-16 20:07:19 -0800

    intro commit

---
+++ server/auth/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const router = express.Router();
+
+// any route in here is pre-pended with /auth
+
+router.get('/', (req, res) => {
+    res.json({
+        message: '🔐'
+    })
+});
+
+// Post route for /auth/signup
+router.post('/signup', (req, res) => {
+    console.log('body', req.body);
+    
+    res.json({
+        message: '✅'
+    });
+});
+
+module.exports = router;
